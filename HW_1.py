71ff82d9b5032d755a4375c10d7e5501aa216742
Beca81 <brankicajovanovic81@gmail.com>
2020-03-09 14:32:33 +0100

    Requested changes added

--- HW_1.py
+++ HW_1.py
@@ -4,8 +4,8 @@ Created on Tue May 21 17:46:24 2019
 
 @author: brank_000
 """
-#Correlation between 
-#the number of deaths associated with influenza and the average temperature
+#correlation between the number of deaths associated with influenza and the average temperature
+#Pearson's correlation
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,7 +20,8 @@ pneum = pd.read_csv('flu_pneumonia.CSV')
 merged = pd.merge(temp, pneum, on="state", how="inner")
 
 
-#Convert Celsius to Fahrenhait --> Return Celsius conversion of input
+#convert Celsius to Fahrenhait --> Return Celsius conversion of input
+#math correlation between F and C --> F = C × 1.8 + 32 
 def celsius_to_fahr(temp_celsius):
     temp_fahr = 9/5*temp_celsius + 32
     return temp_fahr
@@ -29,11 +30,12 @@ celsius_to_fahr(merged["avg_celsius"])
 #new column "avg_fahreinheit"
 merged["avg_fahrenheit"] = celsius_to_fahr(merged["avg_celsius"])
 
-death_rate = merged.iloc[:, 2].values #independent variable vector
-death_abs = merged.iloc[:, 3].values #independent variable vector
+
+#create a function to interpret the results
+death_rate = merged.iloc[:, 2].values       #independent variable vector
+death_abs = merged.iloc[:, 3].values        #independent variable vector
 avg_fahrenheit = merged.iloc[:, 4].values   #dependent variable vector
 
-# create a function to interpret the results
 def interpret_corrcoef(value):
     if (value == -1):
         print ('perfect negative')
@@ -54,16 +56,19 @@ def interpret_corrcoef(value):
     elif (value == 1):
         print ('perfect positive')
     else:
-        print ('error')
+        print ('error')# if value is not between -1 and 1
         
-# correlation
+#plot --> correlation between the number of deaths-absolute associated with influenza and the average temperature
 personScoreFahrDeathAbs=scipy.stats.pearsonr(avg_fahrenheit, death_abs)      
 interpret_corrcoef(personScoreFahrDeathAbs[0])
 sns.regplot( data=merged, x='avg_fahrenheit', y='death_abs')
 
-# correlation
+#plot --> correlation between the number of deaths-rate associated with influenza and the average temperature
 personScoreFahrDeathRate=scipy.stats.pearsonr(avg_fahrenheit, death_rate)      
 interpret_corrcoef(personScoreFahrDeathRate[0])
 plt.figure() # create new window / blank chart
 sns.regplot( data=merged, x='avg_fahrenheit', y='death_rate')
 
+#math correlation between C and F --> C = (5/9) * (F - 32)
+
+plt.show()
