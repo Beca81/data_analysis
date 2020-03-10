4d25518a0934d19d958231385a02181ed382c461
Beca81 <brankicajovanovic81@gmail.com>
2020-03-10 22:06:52 +0100

    Updated Readme.md, added folder images

--- DeathRateAndTemperatureCorr.py
+++ DeathRateAndTemperatureCorr.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue May 21 17:46:24 2019
 
-@author: brank_000
-"""
 #correlation between the number of deaths associated with influenza and the average temperature
 #Pearson's correlation
 
@@ -14,27 +10,11 @@ import pandas as pd
 import scipy
 import scipy.stats
 
-temp = pd.read_csv('avg_winter_temperature.csv')
-pneum = pd.read_csv('flu_pneumonia.CSV')
-
-merged = pd.merge(temp, pneum, on="state", how="inner")
-
-
 #convert Celsius to Fahrenhait --> Return Celsius conversion of input
 #math correlation between F and C --> F = C × 1.8 + 32 
 def celsius_to_fahr(temp_celsius):
     temp_fahr = 9/5*temp_celsius + 32
     return temp_fahr
-celsius_to_fahr(merged["avg_celsius"])
-
-#new column "avg_fahreinheit"
-merged["avg_fahrenheit"] = celsius_to_fahr(merged["avg_celsius"])
-
-
-#create a function to interpret the results
-death_rate = merged.iloc[:, 2].values       #independent variable vector
-death_abs = merged.iloc[:, 3].values        #independent variable vector
-avg_fahrenheit = merged.iloc[:, 4].values   #dependent variable vector
 
 def interpret_corrcoef(value):
     if (value == -1):
@@ -57,18 +37,42 @@ def interpret_corrcoef(value):
         print ('perfect positive')
     else:
         print ('error')# if value is not between -1 and 1
+
+#begin program
+#avg_winter_temperature.csv - the average temperature during the winter season from 2015/2016 by State; 
+temp = pd.read_csv('avg_winter_temperature.csv')
+#flu_pneumonia.CSV - the number of deaths and the death rate associated with the Influenza virus by State in 2015; 
+pneum = pd.read_csv('flu_pneumonia.csv')
+# merged two tables on state(common column)
+merged = pd.merge(temp, pneum, on="state", how="inner")
+
+celsius_to_fahr(merged["avg_celsius"])
+
+# add new column "avg_fahreinheit" as a task from homework
+merged["avg_fahrenheit"] = celsius_to_fahr(merged["avg_celsius"])
+
+
+#create a function to interpret the results
+death_rate = merged.iloc[:, 2].values       #independent variable vector
+death_abs = merged.iloc[:, 3].values        #independent variable vector
+avg_fahrenheit = merged.iloc[:, 4].values   #dependent variable vector
+
+
         
 #plot --> correlation between the number of deaths-absolute associated with influenza and the average temperature
-personScoreFahrDeathAbs=scipy.stats.pearsonr(avg_fahrenheit, death_abs)      
+# --> returns - rfloat(Pearson’s correlation coefficient) in [0] column and
+# p-valuefloat(two-tailed p-value) in [1] column
+personScoreFahrDeathAbs=scipy.stats.pearsonr(avg_fahrenheit, death_abs) 
+#we take [0] column - rfloat(Pearson’s correlation coefficient)     
 interpret_corrcoef(personScoreFahrDeathAbs[0])
+plt.title('Correlation between the number of deaths-absolute associated with influenza and the average temperature')
 sns.regplot( data=merged, x='avg_fahrenheit', y='death_abs')
 
 #plot --> correlation between the number of deaths-rate associated with influenza and the average temperature
 personScoreFahrDeathRate=scipy.stats.pearsonr(avg_fahrenheit, death_rate)      
 interpret_corrcoef(personScoreFahrDeathRate[0])
 plt.figure() # create new window / blank chart
+plt.title('Correlation between the number of deaths-rate associated with influenza and the average temperature')
 sns.regplot( data=merged, x='avg_fahrenheit', y='death_rate')
-
-#math correlation between C and F --> C = (5/9) * (F - 32)
 
 plt.show()
--- README.md
+++ README.md
@@ -1,2 +1,72 @@
+
+#Analysis of data
+
+##**Analysis**
+
+
+<img src="images/flu.jpg">Influenza (flu) viruses can be detected year-round in the United States. But historical data from the CDC has evidence that there is a seasonal component in the spread of the disease, being much more common during the fall and winter.
+
+CDC stands for Center for Disease Control and Prevention. It is one of the major operating components of the Department of Health and Human Services of the United States.
+
+At the first sight, a question pops up: is the cold responsible for you to get sick?
+
+You are a data scientist helping the CDC to answer this question. There are two datasets available for this analysis.
+
+1. The number of deaths and the death rate associated with the Influenza virus by State in 2015;
+
+2. The average temperature during the winter season from 2015/2016 by State;
+
+Disclaimer: 
+
+    The number of deaths and death rate associated with the influenza was downloaded from the CDC;
+    Death rate is the number of deaths by 100,000 of population;
+    The average temperature is an estimate roughly calculated to this case study;
+
+**My observation:**
+
+When I merged two tables, I created a function that receives a temperature in Celsius and converts it to Fahrenheit and add new row – avg.Fahrenheit. 
+
+Then I calculated the Pearson’s correlation coefficient to determine if the number of deaths associated with influenza and the average temperature are correlated.
+
+I did the same thing to the death rate, instead of absolute numbers.
+
+The outcome was :
+
+    correlation the death rate-the average temperature : weak positive
+<p align="center">
+<img src="images/Figure_1.png">
+</p>
+
+    correlation the death abs-the average temperature : no relationships
+<p align="center">
+<img src="images/Figure_2.png">
+</p>
+
+Conclusion:
+
+There is **no statistically significant correlation** between the number of deaths and the average temperature, so the cold is not responsible for you to get sick.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # data_analysis
 https://brankicajovanovic81.wordpress.com/analysis-of-data/
